916dcbe40b275f9b8bafbe8927d8751e7ee8cfdd
ShaqNew <shaquillenewell@gmail.com>
2023-03-15 14:05:30 +0000

    Updated Welcome message

--- frontend/src/Components/welcomeMessage.tsx
+++ frontend/src/Components/welcomeMessage.tsx
@@ -4,6 +4,8 @@ const WelcomeMessage = ()=> {
           <p>
             Please enter an ID, outcode, or Street to find related properties
             <br></br>
+            Try searching for an outcode like E1 or E11 or a Streetname like Primerose Road
+            <br></br>
             Then select the property to see related transactions
           </p>
         </div>
